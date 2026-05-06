41c42a9859c3f5e6f0cf9146a4d1adc30c1857c4
Rylan-Hensley <rhensley43@ivytech.edu>
2026-05-05 22:00:56 -0400

    Watchlist Style Update

--- my-app/App.js
+++ my-app/App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
+import WatchlistScreen from "./WatchlistScreen";
 import TopRanked from "./TopRanked"
 import SearchPage from "./SearchPage";
 
@@ -14,10 +15,10 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name = "Top Ranked" component={TopRanked}/>
-        <Tab.Screen name = "Search" component={SearchPage}/>
+        <Tab.Screen name="Top Ranked" component={TopRanked} />
+        <Tab.Screen name="Search" component={SearchPage} />
+        <Tab.Screen name="Watchlist" component={WatchlistScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
--- my-app/styles.js
+++ my-app/styles.js
@@ -7,7 +7,7 @@ export default StyleSheet.create({
   },
   movie: {
     margin: 10,
-    padding: 10,
+    padding: 5,
     borderWidth: 2,
     borderRadius: 25,
     backgroundColor: 'lightgray',
@@ -40,6 +40,7 @@ export default StyleSheet.create({
   },
   searchBoxContainer: {
     borderBottomWidth: 2,
+    backgroundColor: 'lightgray',
   },
   searchBox: {
     backgroundColor: "white",
@@ -62,5 +63,43 @@ export default StyleSheet.create({
     marginTop: 10,
     marginLeft: 10,
     marginRight: 10,
+  },
+  watchlistContainer: {
+    height: '20%',
+    backgroundColor: '#504459',
+    borderTopWidth: 2
+  },
+  sectionTitle: {
+    backgroundColor: 'lightgray',
+    borderBottomWidth: 2,
+    textAlign: 'center',
+    padding: 10,
+    fontSize: 20
+  },
+  button: {
+    borderRadius: 20,
+    borderWidth: 2,
+    marginTop: 5,
+    width: '30%',
+    alignSelf: 'center',
+    backgroundColor: '#4c729c',
+  },
+  buttonText: {
+    textAlign: 'center',
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  empty: {
+    color: '#FFF',
+    fontSize: 18,
+    textAlign: 'center',
+  },
+  removeButton: {
+    borderRadius: 20,
+    borderWidth: 2,
+    marginTop: 5,
+    width: '30%',
+    alignSelf: 'center',
+    backgroundColor: '#9c5e5a',
   },
 })
